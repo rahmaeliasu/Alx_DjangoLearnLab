772c42ad5a9630520dddc858e457b16b536b456b
Rahma Eliasu <eliasurahma@gmail.com>
2025-11-23 12:51:58 +0000

    update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -57,3 +57,11 @@ class Article(models.Model):
 
     def __str__(self):
         return self.title
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_year = models.IntegerField()
+
+    def __str__(self):
+        return self.title
