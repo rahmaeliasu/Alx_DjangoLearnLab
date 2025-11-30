50f30ac5e8aaecd1e86a4deffcec9f89b7bfef8b
Rahma Eliasu <eliasurahma@gmail.com>
2025-11-30 23:05:29 +0000

    update views

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
+from django_filters import rest_framework 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter, OrderingFilter
 
